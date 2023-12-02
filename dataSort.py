134c18c9da9dc26d9161477a77a64e7c39e5292f
jangdanji <defmaybe69@gmail.com>
2023-12-02 15:57:30 +0900

    Updates

--- dataSort.py
+++ dataSort.py
@@ -6,12 +6,33 @@ import random
 with open('./public/data.json', 'r', encoding='utf-8') as file:
     data = json.load(file)
 
+rawData = """
+101. The British Wildlife Foundation ------- donations last
+week by calling its longtime members.
+(A) solicit
+(B) solicits
+(C) are soliciting
+(D) solicited
+"""
 
+rawData
 
-# 데이터 출력
-for post in data['board']:
+newData = {
+    'quiz': {
+        'quiz-MCQ':[
 
-  post['like'] = 0
+]
+
+        
+    }
+}
+
+data['quiz']['quiz-MCQ']
+
+# # 데이터 출력
+# for post in data['board']:
+
+#   post['like'] = 0
 
 
 pprint.pprint(data)
